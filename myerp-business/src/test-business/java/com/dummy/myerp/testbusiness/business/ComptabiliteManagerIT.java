893fb5b8a80a3c729e07fe35861106098dd34c58
CROS Arthur <cros.arthur@gmail.com>
2020-06-10 14:20:20 +0200

    Adding checkReferenceUnicity ITest

--- myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerIT.java
+++ myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerIT.java
@@ -1,5 +1,10 @@
 package com.dummy.myerp.testbusiness.business;
 
+import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
+import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
+import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
+import com.dummy.myerp.technical.exception.FunctionalException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +12,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,7 +43,33 @@ public class ComptabiliteManagerIT extends BusinessTestCase {
         assertEquals(5, getBusinessProxy().getComptabiliteManager().getListEcritureComptable().size());
     }
 
+    @Test
+    @DisplayName("La référence de l'écriture comptable doit être unique")
+    public void givenEcritureComptable_shouldThrowFunctionalExceptionIfReferenceExists() throws FunctionalException {
+        // GIVEN
+        EcritureComptable vEC = new EcritureComptable();
+        vEC.setJournal(new JournalComptable("AC", "Achat"));
+        vEC.setDate(new GregorianCalendar(2016, Calendar.MARCH, 11).getTime());
+        vEC.setLibelle("Libelle");
+        vEC.setReference("AC-2016/00001");
+        vEC.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null, new BigDecimal(123),
+                null));
+        vEC.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
+                null, null,
+                new BigDecimal(123)));
 
+        // WHEN
+        // ...
+
+        // THEN
+        FunctionalException thrown = assertThrows(
+                FunctionalException.class,
+                () -> { getBusinessProxy().getComptabiliteManager().checkEcritureComptable(vEC); }
+        );
+        assertNotNull(thrown.getMessage());
+        assertTrue(thrown.getMessage().contains("Une autre écriture comptable existe déjà avec la même référence."));
+    }
 
 
 
