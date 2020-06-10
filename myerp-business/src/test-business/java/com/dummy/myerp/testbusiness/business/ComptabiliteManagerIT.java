15c093e7a2d7573e634ee30805e1e95db7950873
CROS Arthur <cros.arthur@gmail.com>
2020-06-10 12:06:57 +0200

    Updating ComptabiliteManagerIT

--- myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerIT.java
+++ myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerIT.java
@@ -3,7 +3,8 @@ package com.dummy.myerp.testbusiness.business;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ComptabiliteManagerIT extends BusinessTestCase {
 
@@ -14,13 +15,19 @@ public class ComptabiliteManagerIT extends BusinessTestCase {
     }
 
     @Test
+    @DisplayName("La liste des journaux comptables de la DB doit contenir 4 lignes")
     public void  gettingListJournalComptable_shouldReturn4() {
         assertEquals(4, getBusinessProxy().getComptabiliteManager().getListJournalComptable().size());
     }
 
     @Test
+    @DisplayName("La liste des écritures comptables de la DB doit contenir 5 lignes")
     public void gettingListEcritureComptable_shouldReturn5() {
         assertEquals(5, getBusinessProxy().getComptabiliteManager().getListEcritureComptable().size());
     }
+
+
+
+
 
 }
